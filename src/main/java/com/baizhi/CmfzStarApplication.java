a916b082200bbbf1f8f14aa2116619a7172657df
Fun610 <57389635+Fun610@users.noreply.github.com>
2019-11-05 17:24:35 +0800

    Update CmfzStarApplication.java

--- src/main/java/com/baizhi/CmfzStarApplication.java
+++ src/main/java/com/baizhi/CmfzStarApplication.java
@@ -9,6 +9,12 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class CmfzStarApplication {
 
     public static void main(String[] args) {
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        System.out.println("-------------------");
         SpringApplication.run(CmfzStarApplication.class, args);
     }
 
